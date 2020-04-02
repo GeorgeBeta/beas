3991f08cd747dfb79bd9e5cedd2d250eb469fc73
GeorgeBeta <jorge@zaporta.com>
2020-04-02 18:05:04 +0200

    Auth - 4 LoginRegister No Funcional

--- server/server.js
+++ server/server.js
@@ -3,8 +3,14 @@ const bodyPaser = require('body-parser');
 require('./config/config');
 const mysql = require('mysql');
 const path = require('path');
+const hbs = require('hbs');
+require('../views/helpers/helpers');
 
 const app = express();
+
+// Express HBS engine
+hbs.registerPartials(path.resolve(__dirname, '../views/partials'));
+app.set('view engine', 'hbs');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyPaser.urlencoded({ extended: false }));
@@ -14,9 +20,18 @@ app.use(express.static(path.resolve(__dirname, '../public')));
 
 app.use(require('./routes/index'));
 
+// Definicioón de las rutas de navegación
 app.get('/', function(req, res) {
-    res.json('Hola la Aplicación ha arrancado');
+    res.render('home');
 });
+
+app.get('/about', function(req, res) {
+    res.render('about');
+})
+
+app.get('/signin', function(req, res) {
+    res.render('signin');
+})
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando por el puerto', process.env.PORT);
